0e2d528d3ae994796d9575eb9cf57d117eefa1c2
vipul-dalal <rudradalal0082004@gmail.com>
2024-03-19 11:21:25 +0530

    Updates

--- js/videosShow.js
+++ js/videosShow.js
@@ -10,60 +10,91 @@ let isContentVisible = false;
 const form = document.forms["form"];
 const optionSelection = form.categorySelection.options;
 form.onchange = function (e) {
-   e.preventDefault();
-   let opt = this.categorySelection.value;
+  e.preventDefault();
+  let opt = this.categorySelection.value;
 
-   singleVidButtons.addEventListener("click", () => {
-      if (isContentVisible || opt === "english" || opt === "hindi") {
-         videoContainer.style.display = "none";
-         contSelection.innerHTML = `<div class="content-type" id="content-type">
+  singleVidButtons.addEventListener("click", () => {
+    if (isContentVisible || opt === "english" || opt === "hindi") {
+      videoContainer.style.display = "none";
+      contSelection.innerHTML = `<div class="content-type" id="content-type">
+                    <button class="btns" onclick="popupMsg()" id="uiux">UI/UX Design</button>
                     <button class="btns" id="webdev">Web Development</button>
                     <button class="btns" id="others">Other Languages</button>
-                    <button class="btns" onclick="popupMsg()" id="appscript">App-Script (Google Sheets)</button>
+                    <button class="btns" id="appscript">App-Script (Google Sheets)</button>
                </div>
                <hr class="lineButton">
                <div class="content-type two" id="main-content-type"></div>`;
-               const others =  document.getElementById("others");
-               others.addEventListener("click", showOtherLanguagesContent);
-               others.scrollIntoView();
-               const webdev = document.getElementById("webdev");
-               webdev.addEventListener("click", showWebDevContent);
-               webdev.scrollIntoView();
-      } else {
-         contSelection.innerHTML = "";
-         videoContainer.style.display = "none";
-      }
-      isContentVisible = !isContentVisible;
-   });
-
-   function showWebDevContent() {
-      document.getElementById(
-         "main-content-type"
-      ).innerHTML = `<button class="btns" id="frontend">Front-End Developement</button>
-          <button class="btns" id="backend" onclick="popupMsg()">Back-End Development</button>`;
-      videoContainer.style.display = "none";
-       const frontend = document.getElementById("frontend");
-       frontend.addEventListener("click", showFrontendContent);
-       frontend.scrollIntoView();
-       const videoCat = document.getElementById("video-cat");
-       videoCat.scrollIntoView();
-       
-      const backend = document.getElementById("backend");
-      backend.addEventListener("click", popupMsg);
-      backend.scrollIntoView();
-   }
-
-
-   if (opt === "english") {
-      catButtons.style.display = "block";
-      videoContainer.style.display = "none";
+      const googleSheets = document.getElementById("appscript");
+      googleSheets.addEventListener("click", showGoogleSheetsContent);
+      const others = document.getElementById("others");
+      others.addEventListener("click", showOtherLanguagesContent);
+      others.scrollIntoView();
+      const webdev = document.getElementById("webdev");
+      webdev.addEventListener("click", showWebDevContent);
+      webdev.scrollIntoView();
+    } else {
       contSelection.innerHTML = "";
+      videoContainer.style.display = "none";
+    }
+    isContentVisible = !isContentVisible;
+  });
+
+  function showWebDevContent() {
+    document.getElementById(
+      "main-content-type"
+    ).innerHTML = `<button class="btns" id="frontend">Front-End Developement</button>
+          <button class="btns" id="backend" onclick="popupMsg()">Back-End Development</button>`;
+    videoContainer.style.display = "none";
+    const frontend = document.getElementById("frontend");
+    frontend.addEventListener("click", showFrontendContent);
+    frontend.scrollIntoView();
+    const videoCat = document.getElementById("video-cat");
+    videoCat.scrollIntoView();
+
+    const backend = document.getElementById("backend");
+    backend.addEventListener("click", popupMsg);
+    backend.scrollIntoView();
+  }
+
+  if (opt === "english") {
+    catButtons.style.display = "block";
+    videoContainer.style.display = "none";
+    contSelection.innerHTML = "";
     //   console.log("English");
 
-      function showOtherLanguagesContent() {
-         document.getElementById("main-content-type").innerHTML = "";
-         videoContainer.style.display = "block";
-         videoContainer.innerHTML = `<div class="note">
+    function showGoogleSheetsContent() {
+      document.getElementById("main-content-type").innerHTML = "";
+      videoContainer.style.display = "block";
+      videoContainer.innerHTML = `<div class="note">
+              <h3>These all videos are available only in <span>English Language</span></h3>
+            </div>
+            <div class="video-cat" id="video-cat">
+                <div class="category show" id="other-cat">
+                    <div class="heading-text">
+                        <h2 class="otherlang">Google Sheets</h2>
+                    </div>
+                    <hr>
+                    <div class="video-content-cont">
+                        <div class="video-container">
+                            <div class="video-category">
+                                <h2>Appscript Language</h2>
+                            </div>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=ZSCwfggOKka14hTo&amp;list=PLv9Pf9aNgemv62NNC5bXLR0CzeaIj5bcw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <div class="video-title">
+                                <h3>C Language Beginner to Professional by <a class="credits" href="https://www.youtube.com/@excelGoogleSheets" target="_blank">Learn Google Sheets & Excel Spreadsheets</a></h3>
+                                <h3><a class="credits" href="https://www.youtube.com/playlist?list=PLv9Pf9aNgemv62NNC5bXLR0CzeaIj5bcw">Full Playlist Check from Here</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+    }
+
+    function showOtherLanguagesContent() {
+      document.getElementById("main-content-type").innerHTML = "";
+      videoContainer.style.display = "block";
+      videoContainer.innerHTML = `<div class="note">
               <h3>These all videos are available only in <span>English Language</span></h3>
             </div>
             <div class="video-cat" id="video-cat">
@@ -102,6 +133,27 @@ form.onchange = function (e) {
                         </div>
                         <div class="video-container">
                             <div class="video-category">
+                                <h2>C# Language</h2>
+                            </div>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/wxznTygnRfQ?si=HwPBUZKkOrrAO7w9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <div class="video-title">
+                                <h3>C# Beginner to Professional by <a class="credits" href="https://www.youtube.com/@BroCodez" target="_blank">Bro Code</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="video-content-cont">
+                        <div class="video-container">
+                            <div class="video-category">
+                                <h2>Data Structure & Algorithms using C</h2>
+                            </div>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/CBYHwZcbD-s?si=buFFkepiMlai8XU0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <div class="video-title">
+                                <h3>Data Structure & Algorithms Beginner to Professional by <a class="credits"
+                                        href="https://www.youtube.com/@BroCodez" target="_blank">Bro Code</a></h3>
+                            </div>
+                        </div>
+                        <div class="video-container">
+                            <div class="video-category">
                                 <h2>Java Language</h2>
                             </div>
                             <iframe width="560" height="315"
@@ -114,8 +166,6 @@ form.onchange = function (e) {
                                         href="https://www.youtube.com/@BroCodez" target="_blank">Bro Code</a></h3>
                             </div>
                         </div>
-                    </div>
-                    <div class="video-content-cont">
                         <div class="video-container">
                             <div class="video-category">
                                 <h2>Python</h2>
@@ -135,11 +185,11 @@ form.onchange = function (e) {
                 </div>
             </div>
             `;
-      }
+    }
 
-      function showFrontendContent() {
-         videoContainer.style.display = "block";
-         videoContainer.innerHTML = `<div class="note">
+    function showFrontendContent() {
+      videoContainer.style.display = "block";
+      videoContainer.innerHTML = `<div class="note">
               <h3>These all videos are available only in <span>English Language</span></h3>
             </div>
             <div class="video-cat">
@@ -257,17 +307,46 @@ form.onchange = function (e) {
                     </div>
                 </div>
             </div>`;
-      }
-   } else if (opt === "hindi") {
-      catButtons.style.display = "block";
-      videoContainer.style.display = "none";
-      contSelection.innerHTML = "";
-      console.log("Hindi");
+    }
+  } else if (opt === "hindi") {
+    catButtons.style.display = "block";
+    videoContainer.style.display = "none";
+    contSelection.innerHTML = "";
+    console.log("Hindi");
 
-      function showOtherLanguagesContent() {
-         document.getElementById("main-content-type").innerHTML = "";
-         videoContainer.style.display = "block";
-         videoContainer.innerHTML = `<div class="note">
+    function showGoogleSheetsContent() {
+        document.getElementById("main-content-type").innerHTML = "";
+        videoContainer.style.display = "block";
+        videoContainer.innerHTML = `<div class="note">
+                <h3>These all videos are available only in <span>English Language</span></h3>
+              </div>
+              <div class="video-cat" id="video-cat">
+                  <div class="category show" id="other-cat">
+                      <div class="heading-text">
+                          <h2 class="otherlang">Google Sheets</h2>
+                      </div>
+                      <hr>
+                      <div class="video-content-cont">
+                          <div class="video-container">
+                              <div class="video-category">
+                                  <h2>Appscript Language</h2>
+                              </div>
+                              <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=ZSCwfggOKka14hTo&amp;list=PLv9Pf9aNgemv62NNC5bXLR0CzeaIj5bcw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                              <div class="video-title">
+                                  <h3>C Language Beginner to Professional by <a class="credits" href="https://www.youtube.com/@excelGoogleSheets" target="_blank">Learn Google Sheets & Excel Spreadsheets</a></h3>
+                                  <h3><a class="credits" href="https://www.youtube.com/playlist?list=PLv9Pf9aNgemv62NNC5bXLR0CzeaIj5bcw">Full Playlist Check from Here</a></h3>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              `;
+      }
+
+    function showOtherLanguagesContent() {
+      document.getElementById("main-content-type").innerHTML = "";
+      videoContainer.style.display = "block";
+      videoContainer.innerHTML = `<div class="note">
                 <h3>These all videos are available only in <span>Hindi Language</span></h3>
               </div>
               <div class="video-cat">
@@ -317,11 +396,11 @@ form.onchange = function (e) {
                       </div>
                   </div>
               </div>`;
-      }
+    }
 
-      function showFrontendContent() {
-         videoContainer.style.display = "block";
-         videoContainer.innerHTML = `<div class="note">
+    function showFrontendContent() {
+      videoContainer.style.display = "block";
+      videoContainer.innerHTML = `<div class="note">
                 <h3>These all videos are available only in <span>Hindi Language</span></h3>
               </div>
               <div class="video-cat">
@@ -358,11 +437,11 @@ form.onchange = function (e) {
                       </div>
                   </div>
               </div>`;
-      }
-   } else {
-      catButtons.style.display = "none";
-      videoContainer.style.display = "none";
-      contSelection.innerHTML = "";
-      console.log("Select Language");
-   }
+    }
+  } else {
+    catButtons.style.display = "none";
+    videoContainer.style.display = "none";
+    contSelection.innerHTML = "";
+    console.log("Select Language");
+  }
 };
