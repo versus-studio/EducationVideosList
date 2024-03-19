5040393b062b48fbf1dd209bce7f610b469673f0
vipul-dalal <rudradalal0082004@gmail.com>
2024-03-19 11:55:17 +0530

    Update videosShow.js

--- js/videosShow.js
+++ js/videosShow.js
@@ -83,7 +83,7 @@ form.onchange = function (e) {
                     <div class="video-content-cont">
                         <div class="video-container">
                             <div class="video-category">
-                                <h2>Appscript Language</h2>
+                                <h2>UI/UX Design</h2>
                             </div>
                             <iframe width="560" height="315" src="https://www.youtube.com/embed/BU_afT-aIn0?si=z2zSVuOHHjuGkWO1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             <div class="video-title">
